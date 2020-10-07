fae2cb7f474035372d586dcc197eadc0c734d31e
kms-uzuki <kensuzuki17@gmail.com>
2020-10-07 15:49:35 -0700

    not much

--- openCV_test.py
+++ openCV_test.py
@@ -3,20 +3,38 @@ import matplotlib
 import cv2
 # import random
 
+def nothing(x):
+	pass
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+
+#img = cv2.imread("hummbird.png", cv2.IMREAD_REDUCED_COLOR_2)
 image = cv2.VideoCapture(0)
+
 
 while (1):
 	retval, img = image.read()
 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	# define range of blue color in HSV
-	# randnum_lower = random.randint(0,350)
-	# randnum_upper = randnum_lower + 1
-	# if randnum_upper > 349:
-	# 	randnum_lower = 349
+	# openCV uses scale to 179 for hue and 255 for saturation/luminance 
 
-	lower = np.array([50,50,50])
-	upper = np.array([150,205,205])
+	l_h = cv2.getTrackbarPos("LH", "Tracking")
+	l_s = cv2.getTrackbarPos("LS", "Tracking")
+	l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+	u_h = cv2.getTrackbarPos("UH", "Tracking")
+	u_s = cv2.getTrackbarPos("US", "Tracking")
+	u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+	lower = np.array([l_h, l_s, l_v])
+	upper = np.array([u_h, u_s, u_v])
 
 	# Threshold the HSV image to get only blue colors
 	mask = cv2.inRange(hsv, lower, upper)
@@ -27,6 +45,8 @@ while (1):
 	cv2.imshow('frame',img)
 	cv2.imshow('mask',mask)
 	cv2.imshow('res',res)
+
 	k = cv2.waitKey(50) & 0xFF
 	if k == 27:
 		break
+cv2.destroyAllWindows()
