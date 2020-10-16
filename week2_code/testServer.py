53e4dc5441f7fc509bbd0cba7b6f1089d2c0f50d
kms-uzuki <kensuzuki17@gmail.com>
2020-10-15 17:50:12 -0700

    yep!

---
+++ week2_code/testServer.py
@@ -0,0 +1,19 @@
+import socket
+import time
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Assigns a port for the server that listens to clients connecting to this port.
+serv.bind(('localhost', 8080))
+serv.listen(5)
+while True:
+    conn, addr = serv.accept()
+    from_client = ''
+    while True:
+        data = conn.recv(4096)
+        if not data: break
+        from_client += data.decode('utf-8')
+        print(from_client)
+        conn.send("I am SERVER\n".encode())
+    conn.close()
+    break
+print('client disconnected')
