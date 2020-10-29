33e7b00b355e8af1737297ab4b402f43d47cd4cc
kms-uzuki <kensuzuki17@gmail.com>
2020-10-29 07:23:31 +0000

    continuing redoing files

--- week3_code/accel.py
+++ week3_code/accel.py
@@ -1,9 +1,16 @@
 import board
 import busio
 import adafruit_lsm9ds1
-
+import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
 imu = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
-
+while (1):
+	ax,ay,az = imu.acceleration
+	gx,gy,gz = imu.gyro
+	t = imu.temperature
+	
+	x = f"accel: ({ax},{ay},{az})"
+	print(x, end='\r', flush=True)
+	time.sleep(0.2)
